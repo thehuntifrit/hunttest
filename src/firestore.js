27add91b842c4285d167d3e9afb720a891f918b0
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-19 10:20:43 +0900

    Update firestore.js

--- src/firestore.js
+++ src/firestore.js
@@ -1,7 +1,7 @@
 // firestore.js
 import { db } from "./firebase.js";
 import { getState } from "./store.js";
-import { DOMElements } from "./modal.js";
+import { DOMElements, closeReportModal } from "./modal.js";
 import { displayStatus } from "./utils.js";
 import { collection, addDoc, onSnapshot, doc } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
