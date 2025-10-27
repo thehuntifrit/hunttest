c26a0f0969dc833d63a7c6596ffca9130a359e38
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-10-27 18:28:55 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -1,6 +1,6 @@
 // uiRender.js
 
-import { calculateRepop, findNextSpawnTime, formatDurationHM, formatLastKillTime, debounce } from "./cal.js";
+import { calculateRepop, findNextSpawnTime, formatDuration, formatDurationHM, formatLastKillTime, debounce } from "./cal.js";
 import { drawSpawnPoint } from "./location.js";
 import { getState, RANK_COLORS, PROGRESS_CLASSES, FILTER_TO_DATA_RANK_MAP } from "./dataManager.js";
 import { renderRankTabs, renderAreaFilterPanel, updateFilterUI, filterMobsByRankAndArea } from "./filterUI.js";
