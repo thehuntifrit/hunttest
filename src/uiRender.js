fb2497407d8e0db7369cbe273a1a753a27aab4e2
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-11-01 23:15:56 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -285,23 +285,16 @@ function updateProgressBar(card, mob) {
 
     bar.classList.remove(PROGRESS_CLASSES.P0_60, PROGRESS_CLASSES.P60_80, PROGRESS_CLASSES.P80_100);
     text.classList.remove(PROGRESS_CLASSES.TEXT_NEXT, PROGRESS_CLASSES.TEXT_POP);
-    wrapper.classList.remove(PROGRESS_CLASSES.BLINK_WHITE);
+    wrapper.classList.remove(PROGRESS_CLASSES.MAX_OVER_BLINK);
 
     if (status === "PopWindow") {
-        if (elapsedPercent <= 60) bar.classList.add(PROGRESS_CLASSES.P0_60); 
-        else if (elapsedPercent <= 80) bar.classList.add(PROGRESS_CLASSES.P60_80); 
-        else bar.classList.add(PROGRESS_CLASSES.P80_100);
-        wrapper.classList.add(PROGRESS_CLASSES.BLINK_WHITE);
+        if (elapsedPercent <= 60) bar.classList.add(PROGRESS_CLASSES.P0_60); else if (elapsedPercent <= 80)
+            bar.classList.add(PROGRESS_CLASSES.P60_80); else bar.classList.add(PROGRESS_CLASSES.P80_100);
         text.classList.add(PROGRESS_CLASSES.TEXT_POP);
-
     } else if (status === "MaxOver") {
-        bar.classList.add(PROGRESS_CLASSES.P80_100);
-        bar.style.animation = "none";
-        text.classList.add(PROGRESS_CLASSES.TEXT_POP);
-
-    } else {
-        text.classList.add(PROGRESS_CLASSES.TEXT_NEXT);
-    }
+        bar.classList.add(PROGRESS_CLASSES.P80_100); text.classList.add(PROGRESS_CLASSES.TEXT_POP);
+        wrapper.classList.add(PROGRESS_CLASSES.MAX_OVER_BLINK);
+    } else { text.classList.add(PROGRESS_CLASSES.TEXT_NEXT); }
 }
 
 function updateProgressText(card, mob) {
