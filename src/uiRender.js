6aada2faaac976f5fc915ee82c5f1d9d7bcecc06
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-11-02 12:53:22 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -144,8 +144,8 @@ function createMobCard(mob) {
     <div class="px-2 py-0 text-sm space-y-0.5">
         <div class="flex justify-between items-start flex-wrap">
             <div class="w-full text-right text-xs text-gray-400 pt-1" data-last-kill></div>
-            <div class="w-full text-left text-sm text-gray-300 mb-1">Memo: <span data-last-memo></span></div>
-            <div class="w-full font-semibold text-yellow-300 border-t border-gray-600 mt-1">抽選条件</div>
+            <div class="w-full text-left text-sm text-gray-300">Memo: <span data-last-memo></span></div>
+            <div class="w-full font-semibold text-yellow-300 border-t border-gray-600">抽選条件</div>
             <div class="w-full text-gray-300 text-xs mt-1">${processText(mob.Condition)}</div>
         </div>
         ${mob.Map && rank === 'S' ? `
