5325f15d7abd989010cc4e41247084a6448976fb
thehuntifrit <the.hunt.ifrit@gmail.com>
2025-11-03 11:19:43 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -96,7 +96,7 @@ function createMobCard(mob) {
             displayCountText = ` <span class="text-yellow-400">${pointNumber}番</span>`;
         } else if (remainingCount > 1) {
             isLastOne = false;
-            displayCountText = ` <span class="text-yellow-400">@${remainingCount}個</span>`;
+            displayCountText = ` <span class="text-xs">@</span>${remainingCount}<span class="text-xs">個</span>`;
         }
 
         isLastOne = remainingCount === 1; // ラスト1点の判定は維持
