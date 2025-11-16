3a28f9808574e3619971dc3af5ba93a38df8fa7b
kyle <the.hunt.ifrit@gmail.com>
2025-11-16 10:47:03 +0900

    Update cal.js

--- src/cal.js
+++ src/cal.js
@@ -425,7 +425,7 @@ function findNextConditionWindow(mob, pointSec, minRepopSec, limitSec) {
           return {
             windowStart: intersectStart,
             windowEnd: intersectEnd,
-            popTime: intersectStart,
+            popTime: weatherResult.popTime,
             remainingSec: 0
           };
         }
