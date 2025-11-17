4af5a6b3ebc241e870f17f5b82ad2123e4dde444
kyle <the.hunt.ifrit@gmail.com>
2025-11-18 03:05:41 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -162,7 +162,7 @@ function createMobCard(mob) {
     <div class="px-2 py-0 text-sm space-y-0.5">
         <div class="flex justify-between items-start flex-wrap">
             <div class="w-full text-right text-xs text-gray-400 pt-1" data-last-kill></div>
-            <div class="mob-memo-row text-sm text-gray-300"><span class="mr-1">Memo:</span><span data-last-memo data-mob-no="..."></span></div>
+            <div class="mob-memo-row text-sm text-gray-300"><span class="mr-1">Memo:</span><span data-last-memo data-mob-no="${mob.No}"></span></div>
             <div class="w-full font-semibold text-yellow-300 border-t border-gray-600">条件</div>
             <div class="w-full text-gray-300 text-xs mt-1">${processText(mob.Condition)}</div>
         </div>
