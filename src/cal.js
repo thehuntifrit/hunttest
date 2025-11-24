83711e04089dbce227f3099ed6f3418f4786bb99
kyle <the.hunt.ifrit@gmail.com>
2025-11-24 12:06:04 +0900

    Update cal.js

--- src/cal.js
+++ src/cal.js
@@ -475,7 +475,7 @@ function calculateRepop(mob, maintenance) {
   } else {
     status = "PopWindow";
     elapsedPercent = Math.min(((now - minRepop) / (maxRepop - minRepop)) * 100, 100);
-    timeRemaining = `🆙 ${formatDurationHM(maxRepop - now)}`;
+    timeRemaining = `⌛️ ${formatDurationHM(maxRepop - now)}`;
   }
 
   if (isInConditionWindow && now >= minRepop) {
