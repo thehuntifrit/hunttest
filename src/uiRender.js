8697d6fbcfa22d31c3fe40ec7636b724fa09cad4
kyle <the.hunt.ifrit@gmail.com>
2025-11-25 13:42:29 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -66,10 +66,10 @@ function createMobCard(mob) {
       isLastOne = true;
       const pointId = validSpawnPoints[0]?.id || "";
       const pointNumber = pointId.slice(-2);
-      displayCountText = ` <span class="text-xs text-yellow-400 font-bold text-glow">${pointNumber}</span><span class="text-xs text-yellow-400 relative -top-[0.02rem]">番</span>`;
+      displayCountText = ` <span class="text-xs text-yellow-400 font-bold text-glow">${pointNumber}番</span>`;
     } else if (remainingCount > 1) {
       isLastOne = false;
-      displayCountText = ` <span class="text-xs text-gray-400 relative -top-[0.1rem]">&nbsp;@</span>&nbsp;<span class="text-xs text-gray-400 relative top-[0.02rem]">${remainingCount}</span>`;
+      displayCountText = ` <span class="text-xs text-gray-400 relative -top-[0.09rem]">&nbsp;@</span><span class="text-xs text-gray-400 relative top-[0.02rem]">${remainingCount}</span>`;
     }
 
     isLastOne = remainingCount === 1;
