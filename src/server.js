4bba51da17f8012ac28de600462faa8deeb6ba3a
kyle <the.hunt.ifrit@gmail.com>
2025-11-26 01:47:48 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -234,7 +234,5 @@ const toggleCrushStatus = async (mobNo, locationId, nextCulled) => {
     }
 };
 
-export { 
-    initializeAuth, subscribeMobStatusDocs, subscribeMobLocations, subscribeMobMemos, 
-    submitReport, submitMemo, toggleCrushStatus
-};
+export { initializeAuth, subscribeMobStatusDocs, subscribeMobLocations, 
+    subscribeMobMemos, submitReport, submitMemo, toggleCrushStatus };
