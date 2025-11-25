5adcab56758f883101077a65fe24bdcbfefd8f11
kyle <the.hunt.ifrit@gmail.com>
2025-11-25 14:48:05 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -499,7 +499,7 @@ function updateProgressText(card, mob) {
 
   text.innerHTML = `
     <div class="w-full h-full grid grid-cols-2 items-center text-sm font-bold">
-      <div class="pl-1 text-left truncate">${leftStr}${percentStr}</div>
+      <div class="pl-2 text-left truncate">${leftStr}${percentStr}</div>
       <div class="pr-1 text-right truncate">${rightContent}</div>
     </div>
   `;
