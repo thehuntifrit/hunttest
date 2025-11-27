e97503eb00a04e6c033e36fd33ef60fd59745727
kyle <the.hunt.ifrit@gmail.com>
2025-11-28 02:02:38 +0900

    Update uiRender.js

--- src/uiRender.js
+++ src/uiRender.js
@@ -73,8 +73,8 @@ function createMobCard(mob) {
       displayCountText = ` <span class="text-xs text-yellow-400 font-bold text-glow">${pointNumber}番</span>`;
     } else if (remainingCount > 1) {
       isLastOne = false;
-      displayCountText = ` <span class="text-xs text-gray-400 relative -top-[0.09rem]">@</span><span class="text-sm text-gray-400 font-stretch-condensed relative top-[0.02rem]">${remainingCount}</span>`;
-    }
+      displayCountText = ` <span class="text-xs text-gray-400 relative -top-[0.09rem]">@</span><span class="text-sm text-gray-400 relative top-[0.02rem]">${remainingCount}</span>`;
+    } 
     isLastOne = remainingCount === 1;
   }
 
